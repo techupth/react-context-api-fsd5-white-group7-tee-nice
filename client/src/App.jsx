fd78723dc6e54ad5eb99c23f7cbfbe9cd73a4ffc
mortybunyarit <bunyarit.saelim@gmail.com>
2024-01-08 15:50:49 +0700

    feat:context_api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
